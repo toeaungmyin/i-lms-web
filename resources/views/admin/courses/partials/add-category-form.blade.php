5934af8d4184266aa5f82eb95d930eb1f70f04f2
toeaungmyin-noobie <toeaungmyin.official@gmail.com>
2024-08-27 11:04:09 +0630

    fix cat

--- resources/views/admin/courses/partials/add-category-form.blade.php
+++ resources/views/admin/courses/partials/add-category-form.blade.php
@@ -18,7 +18,7 @@
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" class="px-6 py-3">
-                                ID
+                                No
                             </th>
                             <th scope="col" class="px-6 py-3">
                                 Category name
@@ -35,7 +35,7 @@
                         @foreach ($categories as $category)
                             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                 <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {{  $category->id }}
+                                    {{  $loop->iteration }}
                                 </th>
                                 <td class="px-6 py-2">
                                     {{  $category->name }}
